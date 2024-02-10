9b121120883a037870fbd79d7aad56714057c3ba
Flayger <36726488+Flayger@users.noreply.github.com>
2024-02-10 17:56:39 +0700

    Add stats. No error handling.

--- src/main/java/org/flayger/FileFilter.java
+++ src/main/java/org/flayger/FileFilter.java
@@ -1,12 +1,13 @@
 package org.flayger;
 
 import java.io.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FileFilter {
 
@@ -47,7 +48,6 @@ public class FileFilter {
     static boolean floatOverride = true;
     static boolean intOverride = true;
     static boolean stringOverride = true;
-
 
 
     static List<String> fileNames = new ArrayList<>();
@@ -95,6 +95,27 @@ public class FileFilter {
     }
                  */
 
+    //short stats
+    private static Integer intsCount = 0;
+    private static Integer floatsCount = 0;
+    private static Integer stringsCount = 0;
+
+    //full stats в отдельный класс? отдельный класс для каждого типа? и создавать экземпляры с методами для увеличения-изменения?
+    private static BigInteger intsMaxValue;
+    private static BigInteger intsMinValue;
+    private static BigInteger intsAverage = new BigInteger("0");
+    private static BigInteger intsSum = new BigInteger("0");
+
+    //float static
+    private static BigDecimal floatsMaxValue;
+    private static BigDecimal floatsMinValue;
+    private static BigDecimal floatsAverage = new BigDecimal("0.0");
+    private static BigDecimal floatsSum = new BigDecimal("0");
+
+    //string
+    private static BigInteger stringsMaxLength;
+    private static BigInteger stringsMinLength;
+
     public static void main(String[] args) {
         argsCommandLineProcessing(args);
 
@@ -106,52 +127,59 @@ public class FileFilter {
 
         }
 
-        /*
-        Pattern[] patterns = Pattern[] {Pattern.compile("^\\d+$"), Pattern.compile("^\\d+\\.\\d*$"), Pattern.compile("^\\d{1,2}/\\d{1,2}/\\d{1,2}$")};
-        Class<?> types = new Class[] {Integer.class, Double.class, Date.class};
-        for (int i - 0;  i < patterns.length;  i++) {
-            if (patterns[i].matches(str)) {
-                return types[i];
-            }
-        }
-        return null;
-         */
         System.out.println("start files");
         System.out.println(fileNames);
         //обработка файлов
-        for (String file : fileNames) {
-            //мб можно распараллелить?
-            System.out.println("open " + file);
-            //ошибка открытия файла?
-            //Scanner scanner = new Scanner(openFile);
-            readFileAndFilterData(file);
+        try (ExecutorService executor = Executors.newFixedThreadPool(10)) {
+            for (String file : fileNames) {
+                executor.submit(() -> readFileAndFilterData(file));
+            }
+            executor.shutdown(); // Shutdown the executor after all tasks are submitted
         }
 
+        showStatistics();
+
+//        for (String file : fileNames) {
+//            //мб можно распараллелить?
+//            System.out.println("open " + file);
+//            //ошибка открытия файла?
+//            //Scanner scanner = new Scanner(openFile);
+//            readFileAndFilterData(file);
+//        }
 
 
     }
 
-    public static void readFileAndFilterData(String file){
+    private static void showStatistics() {
+        System.out.printf("integers stats - count - %S, min - %S, max - %S, average - %S, sum - %S \n", intsCount, intsMinValue, intsMaxValue, intsAverage, intsSum);
+
+        System.out.printf("floats stats - count - %S, min - %S, max - %S, average - %S, sum - %S \n", floatsCount, floatsMinValue, floatsMaxValue, floatsAverage, floatsSum);
+
+        System.out.printf("strings stats - count - %S, minLength - %S, maxLength - %S \n", stringsCount, stringsMinLength, stringsMaxLength);
+    }
+
+    public static void readFileAndFilterData(String file) {
         try (BufferedReader reader = new BufferedReader(new FileReader(file));) {
             String line = reader.readLine();
             while (line != null) {
-
                 // for (int i = 0; i < openFile.length(); i++) {
                 //   = scanner.nextLine();
                 System.out.println("line " + line);
                 //если ошибка в считывании строки - читать следующую
-                if (line.matches("^^-?\\d+\\.\\d*E?.\\d*$")) {
+                if (line.matches("^-?\\d+\\.\\d*E?.\\d*$")) {
                     //с минусом или нет с любым (от 1) количеством цифр и одной точкой и любым количеством цифр после точки до конца строки
                     //Float dataFloat = Float.parseFloat(line);
                     //запись в файл здесь? проверка перезаписи? сделать отдельный метод для записи - передавать в него тип записываемых данных,
                     // данные и уже в том методе определять, в какой файл записывать и какие параметры записи (перезапись или нет)
                     File floatsOutput = new File(outputFilesPath + filePrefix + "floats" + ".txt");
                     saveFloatData(line);
+                    //checkStats(line, Float.class);
                     //если ошибка в записи в файл - ждать или читать следующую строку
                 } else if (line.matches("^-?\\d+$")) {
                     //с минусом или нет с любым (от 1) количеством цифр до конца строки
                     //Integer dataInteger = Integer.parseInt(line);
                     saveIntegerData(line);
+
                 } else {
                     //остаются только строки
                     saveStringData(line);
@@ -231,52 +259,91 @@ public class FileFilter {
     //sout short or short+full statistic
 
 
-    public static void saveFloatData(String data) throws IOException {
+    public static void saveFloatData(String dataFloat) throws IOException {
         //можно сделать универсальный метод, который просто пришедший тип будет разбивать. generics?
         //вставка в конец файла
-        //write data
+        //write dataFloat
         System.out.println("saving float");
         File floatsOutput = new File(outputFilesPath + filePrefix + "floats" + ".txt");
 
-        if(isOverride && floatOverride) {
+        if (isOverride && floatOverride) {
             Files.deleteIfExists(floatsOutput.toPath());
             floatOverride = false;
         }
         floatsOutput.createNewFile();
 //        try (FileWriter myWriter = new FileWriter(floatsOutput)) {
-//            myWriter.write(String.valueOf(data));
+//            myWriter.write(String.valueOf(dataFloat));
 //        }
-        //Files.write(floatsOutput.toPath(), String.valueOf(data).getBytes(), StandardOpenOption.APPEND);
-        try(FileWriter fw = new FileWriter(floatsOutput, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter out = new PrintWriter(bw))
-        {
-            out.println(data);
+        //Files.write(floatsOutput.toPath(), String.valueOf(dataFloat).getBytes(), StandardOpenOption.APPEND);
+        try (FileWriter fw = new FileWriter(floatsOutput, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+            out.println(dataFloat);
         }
+
+        //if short stats
+        floatsCount++;
+        //if full stats
+        //new stringStats if null
+        BigDecimal data = new BigDecimal(dataFloat);
+        if (floatsMaxValue == null) {
+            floatsMaxValue = data;
+        } else if (floatsMaxValue.compareTo(data) < 0) {
+            floatsMaxValue = data;
+        }
+
+        if (floatsMinValue == null) {
+            floatsMinValue = data;
+        } else if (floatsMinValue.compareTo(data) > 0) {
+            floatsMinValue = data;
+        }
+        //avg and sum
+        floatsSum = floatsSum.add(data);
+        floatsAverage = floatsSum.divide(BigDecimal.valueOf(intsCount));
+
     }
 
-    public static void saveIntegerData(String data) throws IOException {
+    public static void saveIntegerData(String dataInteger) throws IOException {
         System.out.println("saving int");
         File integersOutput = new File(outputFilesPath + filePrefix + "integers" + ".txt");
 
-        if(isOverride && intOverride){
+        if (isOverride && intOverride) {
             Files.deleteIfExists(integersOutput.toPath());
             intOverride = false;
         }
         integersOutput.createNewFile();
 //        try (FileWriter myWriter = new FileWriter(integersOutput)) {
-//            myWriter.write(String.valueOf(data));
+//            myWriter.write(String.valueOf(dataInteger));
 //        }
-        //Files.write(integersOutput.toPath(), String.valueOf(data).getBytes(),StandardOpenOption.APPEND);
-        try(FileWriter fw = new FileWriter(integersOutput, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter out = new PrintWriter(bw))
-        {
-            out.println(data);
+        //Files.write(integersOutput.toPath(), String.valueOf(dataInteger).getBytes(),StandardOpenOption.APPEND);
+        try (FileWriter fw = new FileWriter(integersOutput, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+            out.println(dataInteger);
         }
+        //if short stats
+        intsCount++;
+        //if full stats
+        //new stringStats if null
+        BigInteger data = new BigInteger(dataInteger);
+        if (intsMaxValue == null) {
+            intsMaxValue = data;
+        } else if (intsMaxValue.compareTo(data) < 0) {
+            intsMaxValue = data;
+        }
+
+        if (intsMinValue == null) {
+            intsMinValue = data;
+        } else if (intsMinValue.compareTo(data) > 0) {
+            intsMinValue = data;
+        }
+        //avg and sum
+        intsSum = intsSum.add(data);
+        intsAverage = intsSum.divide(BigInteger.valueOf(intsCount));
+
     }
 
-    public static void saveStringData(String data) throws IOException {
+    public static void saveStringData(String dataString) throws IOException {
         System.out.println("saving string");
         File stringsOutput = new File(outputFilesPath + filePrefix + "strings" + ".txt");
         if (isOverride && stringOverride) {
@@ -289,12 +356,28 @@ public class FileFilter {
 //        }
         //(FileWriter myWriter = new FileWriter( stringsOutput)) {
         //Files.write(Paths.get(stringsOutput.toURI()), data.getBytes(), StandardOpenOption.APPEND);
-        try(FileWriter fw = new FileWriter(stringsOutput, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter out = new PrintWriter(bw))
-        {
-            out.println(data);
+        try (FileWriter fw = new FileWriter(stringsOutput, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+            out.println(dataString);
         }
+        //if short stats
+        stringsCount++;
+        //if full stats
+        //new stringStats if null
+        BigInteger lineSize = BigInteger.valueOf(dataString.length());
+        if (stringsMaxLength == null) {
+            stringsMaxLength = lineSize;
+        } else if (stringsMaxLength.compareTo(lineSize) < 0) {
+            stringsMaxLength = lineSize;
+        }
+
+        if (stringsMinLength == null) {
+            stringsMinLength = lineSize;
+        } else if (stringsMinLength.compareTo(lineSize) > 0) {
+            stringsMinLength = lineSize;
+        }
+
     }
 
     public static void argsCommandLineProcessing(String[] args) {
